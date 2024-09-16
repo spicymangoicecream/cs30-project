b23c1abbcf9413a66511acf4cc0c77419f62b3d1
spicymangoicecream <vutuanhuy2007@gmail.com>
2024-09-16 12:16:09 -0600

    Interactive Scene
    set background color: checked

---
+++ Project/sketch.js
@@ -0,0 +1,15 @@
+// Interactive Scene
+// Henry Vu
+// 16.9.2024
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(700, 500);
+}
+
+function draw() {
+  background(0);
+}
