c1713aba51b836ad055a8d92432351b3529ed678
spicymangoicecream <vutuanhuy2007@gmail.com>
2024-10-01 12:16:43 -0600

    Project Perlin Peak

--- 06 Perlin Noise Demo/sketch.js
+++ 06 Perlin Noise Demo/sketch.js
@@ -4,10 +4,14 @@
 
 
 // noise() vs random()
+let circleTime = 5;
+let circleInterval = 0.02; 
+let rectTime = 5;
+let rectInt = 1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  frameRate(2);
+  // frameRate(15);
 
 }
 
@@ -21,17 +25,33 @@ function draw() {
   randomCircle();
 
 
-
-
-
-
-
-
-
-
-
-
   //noise()
+  noiseCircle();
+
+  stairCase();
+}
+
+function noiseCircle(){
+  // draw a circle using noise() for random value
+  let cSize = noise(circleTime); // 0-1
+  cSize = map(cSize, 0, 1, 10, 225);
+  fill(cSize, cSize/2, 225-cSize);
+  circle(width*0.75, height/2, cSize);
+  text(round(cSize), width*0.75, height/2);
+  circleTime += circleInterval; 
+}
+
+function stairCase(){
+  // use loop to draw a terrain 
+  let rectWidth = 20;
+  for(let x = 0; x <= width; x += rectWidth){
+    noFill()
+    let rectHeight = noise(rectTime);
+    rectHeight = map(rectHeight, 0, 1, 50, 500);
+    rect(x, height, rectWidth, -rectHeight);
+    rectTime += rectInt;
+  }
+
 }
 
 function randomCircle(){
@@ -39,6 +59,7 @@ function randomCircle(){
   //random values for the diameter (later,color)
 
   let cSize = random(10,100);
+  fill(cSize, cSize/2, 225-cSize);
   circle(width*0.25, height/2, cSize);
   textAlign(CENTER, CENTER);
   textSize(15);
---
+++ Perlin Peak Project/sketch.js
@@ -0,0 +1,23 @@
+// Terrain Change
+// Henry V
+// 1.10.2024
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+let rectWidth = 1;
+let rectTime = 5;
+let rectInt = 1;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+}
+
+
+function terrain() {
+
+}
