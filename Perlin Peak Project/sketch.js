16359b598cb2048fbe241d676d20a71c4cfa45e6
spicymangoicecream <vutuanhuy2007@gmail.com>
2024-10-02 12:18:37 -0600

    Perlin Project
    added size changing

--- Perlin Peak Project/sketch.js
+++ Perlin Peak Project/sketch.js
@@ -1,23 +1,56 @@
-// Terrain Change
+// Terrain Generation 
 // Henry V
 // 1.10.2024
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
-let rectWidth = 1;
+
+// Variable List 
+let rectWidth = 5;
 let rectTime = 5;
-let rectInt = 1;
+let rectInt = 0.01;
 
+// Programs Start here:
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1000, 1000);
+  frameRate(15);
+  background(220);
+  terrain();
 }
 
 function draw() {
-  background(220);
+  rectTime = 5;
 }
 
+//// TERRAIN GENERATION ////
 
 function terrain() {
+  for (let x = 0; x <= width; x += rectWidth) {
+    noFill();
+    let rectHeight = noise(rectTime);
+    rectHeight = map(rectHeight, 0 , 1, 60, 900);
+    rect(x, height, rectWidth, -rectHeight);
+    rectTime += rectInt; 
+  }
+}
 
+function keyPressed() {
+  if (keyCode === RIGHT_ARROW) {
+    rectWidth += 0.1;
+    background(220);
+    terrain();
+
+    if (rectWidth < 1) {
+      rectWidth = 1;
+  }
+}
+
+if (keyCode === LEFT_ARROW) {
+  rectWidth -= 0.1;
+  background(220);
+  terrain();
+
+  if (rectWidth < 1) {
+    rectWidth = 1;
+}
+}
 }
