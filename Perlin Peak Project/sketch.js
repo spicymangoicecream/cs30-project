c7ab8b55a1b4d3b64fd39f38e415c11895a2ae1b
spicymangoicecream <vutuanhuy2007@gmail.com>
2024-10-08 12:16:57 -0600

    Perlin Final

--- Perlin Peak Project/sketch.js
+++ Perlin Peak Project/sketch.js
@@ -5,28 +5,35 @@
 
 
 // Variable List 
-let rectWidth = 5;
+let rectWidth = 1;
 let rectTime = 5;
 let rectInt = 0.01;
 
 let highestY = 0;
 let rectX = 0;
+let avgY = 0; // variable to calculate the average height of the sequences
 
 // Programs Start here:
 function setup() {
   createCanvas(1000, 1000);
-  frameRate(15);
-  background(220);
-  terrain();
+  frameRate(60);
 }
 
 function draw() {
-  rectTime = 5;
+  background(220);
+  terrain();
+  rectTime = 5; 
+  rectTime += (frameCount/20);
+  stroke(255,0,0);
+  line(0, avgY, width, avgY);
+  avgY = 0; 
+  stroke(0);
 }
 
 //// TERRAIN GENERATION ////
 
 function terrain() {
+  highestY = 0;
   for (let x = 0; x <= width; x += rectWidth) {
     noFill();
     let rectHeight = noise(rectTime);
@@ -38,11 +45,11 @@ function terrain() {
       highestY = abs(rectHeight);
       rectX = x;
     }
-
-    drawFlag();
+    avgY += rectHeight/(width/rectWidth);
   }
+  drawFlag(rectX, height - highestY);
 }
-
+ // key function to change the width 
 function keyPressed() {
   if (keyCode === RIGHT_ARROW) {
     rectWidth += 0.1;
@@ -66,7 +73,8 @@ if (keyCode === LEFT_ARROW) {
 }
 
 /// FLAG /// 
-function drawFlag() {
+function drawFlag(x,y) {
+  rect(rectX, height - highestY, 3, -20);
   fill(255, 0, 0);
-  rect(rectX, height - highestY, 10);
+  rect(rectX, height - highestY -10, 15, -10);
 }
