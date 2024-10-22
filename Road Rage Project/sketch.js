e4c338bec8b7cf2fdf8f64802cc6873644751e88
spicymangoicecream <vutuanhuy2007@gmail.com>
2024-10-22 12:16:12 -0600

    Road Rage updated
    add move and speed, added boundaries

--- Road Rage Project/sketch.js
+++ Road Rage Project/sketch.js
@@ -2,6 +2,9 @@
 // Henry V
 // 10.18.2024
 
+let eastbound = [];
+let westbound = [];
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
@@ -15,20 +18,59 @@ function drawRoad() { //// draw the road
   fill(0);
   rect(0, height/4, width, height/2);
   let lineDist = 30; /// defining the gap between each line 
-  fill(255);
+  fill(255,255,0);
   for (let i = 0; i < width; i++) {
-    rect(0 + i*lineDist, height/2, 20, 5);
+    rect(0 + i*lineDist, height/2, 15, 3);
   }
 }
 
 class Vehicle {
   ////// Constructor //// 
-  constructor(type, color, x, y, xSpeed, direction) {
-    this.x = x;
-    this.y = y; 
-    this.type = 
+  constructor(x, y, direction, type) {
+    this.direction = direction;
     this.color = (random(255), random(255), random(255));
-    this.type;
-    this.xSpeed = 0;
+    this.y = y;
+    this.x = x;
+    this.type = type;
+    this.xSpeed = random(2,5);
+  }
+
+  update () {
+    
+  }
+
+  display() {
+    fill(this.color);
+    if (this.type === 0) {
+      rect(0 , this.y, 20, 10); // basic car shape
+    }
+
+    else {
+      rect(0, this.y, 30, 10);
+    }
+  }
+
+  move() {
+    // this keep the car in frames and moving it at the same time
+    this.x += this.xSpeed;
+
+    if(this.x < 0) {
+      this.x += width;
+    }
+
+    if(this.x > width) {
+      this.x -= width;
+    }
+  }
+
+  speedUp() {
+    // car going west do this
+    if (this.direction === 1) {
+
+    }
+  }
+
+  speedDown() {
+
   }
 }
