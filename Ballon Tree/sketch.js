fecdc62de3d54a04574481954019b4a30a1b0f37
spicymangoicecream <vutuanhuy2007@gmail.com>
2024-11-15 12:15:54 -0600

    Balloon Tree
    make the leaf worked

--- Ballon Tree/sketch.js
+++ Ballon Tree/sketch.js
@@ -2,19 +2,19 @@
 // Your Name
 // Date
 //
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
-
+let seed;
 let scale = 15;
 
 function setup() {
   createCanvas(500, 500);
   background(255);
+  randomSeed(100);
 }
 
 function draw() {
-  drawTree(width/2, height*.9, 90, 6);
+  drawTree(width/2, height*0.9, 90, 6);
+  drawLeaf();
 }
 
 function drawLine( x1, y1, x2, y2, depth) {
@@ -29,11 +29,22 @@ function drawTree(x1, y1, angle, depth) {
     
     let y2 = y1 - (sin(radians(angle))*depth*scale); //using trig ratios. Get
     // shorter based on depth
-    
+
     drawLine(x1, y1, x2, y2, depth);
-    //for a 2-branch tree:
-    
-    drawTree(x2, y2, angle-18, depth-1);
-    drawTree(x2, y2, angle+18, depth-1);
+
+    // draw leaf according to the depth 
+    if (depth < 5) {
+      drawLeaf(x2,y2,depth*6);
+    }
+
+    //for a 3-branch tree:
+    drawTree(x2,y2, angle-35, depth-1);
+    drawTree(x2, y2, angle-0, depth-1);
+    drawTree(x2, y2, angle+35, depth-1);
   }
+}
+
+function drawLeaf(x,y,depth) {
+  fill(random(255),random(255),random(255));
+  circle(x, y, depth);
 }
